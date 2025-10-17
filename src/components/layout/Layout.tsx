dfa262765295cb188301825dfba1708633a1199e
[dyad] <ezequielrodriguez1991@gmail.com>
2025-10-17 14:18:27 -0400

    [dyad] Eliminado el efecto de animación del cursor al hacer clic - wrote 1 file(s), deleted 1 file(s)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
-import { WifiCursorEffect } from '@/components/effects/WifiCursorEffect';
+// Removed import of WifiCursorEffect
 
 export const Layout = () => {
   return (
     <div className="bg-white relative">
-      <WifiCursorEffect />
+      {/* Removed WifiCursorEffect component */}
       <div className="relative z-10 flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
