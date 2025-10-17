4a41b031812e2889d975a0c23207a295ce7dbf9a
[dyad] <ezequielrodriguez1991@gmail.com>
2025-10-16 21:14:12 -0400

    Reverted all changes back to version aa766e8f8f226c4a7ae5c715f316b9b6d72c6dfd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,22 +1,32 @@
-import Hero from "@/components/sections/Hero";
-import TrustBadges from "@/components/sections/TrustBadges";
-import TopDestinations from "@/components/sections/TopDestinations";
-import TravelGuides from "@/components/sections/TravelGuides";
-import Testimonials from "@/components/sections/Testimonials";
-import Cta from "@/components/sections/Cta";
+import { Hero } from "@/components/sections/Hero";
+import { TrustBadges } from "@/components/sections/TrustBadges";
+import { Features } from "@/components/sections/Features";
+import { SocialProof } from "@/components/sections/SocialProof";
+import { HowItWorks } from "@/components/sections/HowItWorks";
+import { Pricing } from "@/components/sections/Pricing";
+import { EsimOrSim } from "@/components/sections/EsimOrSim";
+import { Testimonials } from "@/components/sections/Testimonials";
+import { Coverage } from "@/components/sections/Coverage";
+import { BusinessSolutions } from "@/components/sections/BusinessSolutions";
+import { Faq } from "@/components/sections/Faq";
+import { Cta } from "@/components/sections/Cta";
 
 const Index = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        <Hero />
-        <TrustBadges />
-        <TopDestinations />
-        <TravelGuides />
-        <Testimonials />
-        <Cta />
-      </main>
-    </div>
+    <>
+      <Hero />
+      <TrustBadges />
+      <Features />
+      <SocialProof />
+      <HowItWorks />
+      <Pricing />
+      <EsimOrSim />
+      <Testimonials />
+      <Coverage />
+      <BusinessSolutions />
+      <Faq />
+      <Cta />
+    </>
   );
 };
 
