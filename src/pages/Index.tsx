ceb6edeede8bff8b7ace7c6ac67cfbbd4ff309ca
[dyad] <ezequielrodriguez1991@gmail.com>
2025-10-16 21:01:54 -0400

    [dyad] Corrigiendo error de importación y eliminando Footer duplicado - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,21 +2,17 @@ import Hero from "@/components/sections/Hero";
 import { TrustBadges } from "@/components/sections/TrustBadges";
 import { Features } from "@/components/sections/Features";
 import { Testimonials } from "@/components/sections/Testimonials";
-import { CTA } from "@/components/sections/CTA";
-import { Footer } from "@/components/sections/Footer";
+import { Cta } from "@/components/sections/Cta";
 
 const Index = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        <Hero />
-        <TrustBadges />
-        <Features />
-        <Testimonials />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <Hero />
+      <TrustBadges />
+      <Features />
+      <Testimonials />
+      <Cta />
+    </>
   );
 };
 
