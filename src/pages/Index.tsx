16a1de1f0f3d01d562dd687d8c10700d3dca161a
[dyad] <ezequielrodriguez1991@gmail.com>
2025-10-16 21:05:05 -0400

    Reverted all changes back to version 4a353cb4de2b058cb1673fe8d19ea969216bd089

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,17 +2,21 @@ import Hero from "@/components/sections/Hero";
 import { TrustBadges } from "@/components/sections/TrustBadges";
 import { Features } from "@/components/sections/Features";
 import { Testimonials } from "@/components/sections/Testimonials";
-import { Cta } from "@/components/sections/Cta";
+import { CTA } from "@/components/sections/CTA";
+import { Footer } from "@/components/sections/Footer";
 
 const Index = () => {
   return (
-    <>
-      <Hero />
-      <TrustBadges />
-      <Features />
-      <Testimonials />
-      <Cta />
-    </>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <Hero />
+        <TrustBadges />
+        <Features />
+        <Testimonials />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
   );
 };
 
