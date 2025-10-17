b32871a4b7b374f9c51eb6ee007f0771de7b90da
[dyad] <ezequielrodriguez1991@gmail.com>
2025-10-17 00:07:20 -0400

    [dyad] Changing button icon color - wrote 1 file(s)

--- src/components/ui/get-started-button.tsx
+++ src/components/ui/get-started-button.tsx
@@ -14,7 +14,7 @@ const GetStartedButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
         <span className="mr-8 transition-opacity duration-500 group-hover:opacity-0">
           {children}
         </span>
-        <i className="absolute right-1 top-1 bottom-1 rounded-sm z-10 grid w-1/4 place-items-center transition-all duration-500 bg-primary-foreground/15 group-hover:w-[calc(100%-0.5rem)] group-active:scale-95 text-black">
+        <i className="absolute right-1 top-1 bottom-1 rounded-sm z-10 grid w-1/4 place-items-center transition-all duration-500 bg-primary-foreground/15 group-hover:w-[calc(100%-0.5rem)] group-active:scale-95 text-white">
           <ChevronRight size={16} strokeWidth={2} aria-hidden="true" />
         </i>
       </Button>
